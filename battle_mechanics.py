8509c9c6d42587333052684b676093d092b7da3c
aye-ron98 <aron.zhang013@gmail.com>
2022-11-25 22:59:36 -0800

    Remove character_has_leveled, character_health, execute_glow_up to character conditions.py

--- battle_mechanics.py
+++ battle_mechanics.py
@@ -9,38 +9,6 @@ functions related to fighting
 """
 
 
-def character_has_leveled(character):
-    if character['level'] == 1:
-        if character['xp'] == 5:
-            character['level'] = 2
-            character['xp'] = 0
-            character['hp'] = 10
-            character['glow up'] = True
-    elif character['level'] == 2:
-        if character['xp'] == 10:
-            character['level'] = 3
-            character['xp'] = 0
-            character['hp'] = 15
-            character['glow up'] = True
-            character['level cap'] = True
-
-    return character
-
-
-def character_health(character):
-    if character['hp'] == 0:
-        return True
-    else:
-        return False
-
-
-def execute_glow_up_protocol(character):
-    if character['glow up']:
-        character['glow up'] = False
-        print('glow up, level up')
-    return character
-
-
 def check_for_challenges(character, board):
     if board[(character['location X'], character['location Y'])] == 'empty room':
         return False
