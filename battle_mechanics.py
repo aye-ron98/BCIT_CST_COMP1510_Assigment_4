00082cf5f885100fc5ec80dc43f456c5b8f548c8
aye-ron98 <aron.zhang013@gmail.com>
2022-11-25 22:53:17 -0800

    Remove get user direction to movement.py

--- battle_mechanics.py
+++ battle_mechanics.py
@@ -9,33 +9,6 @@ functions related to fighting
 """
 
 
-def get_user_choice(character):
-    directions = ['move north', 'move east', 'move south', 'move west']
-    for choice, direction in enumerate(directions, 1):
-        print(choice, direction)
-
-    while True:
-        move = input('What direction would you like to move?')
-        if move == '1' and character['location X'] < 4:
-            character['location X'] += 1
-            break
-        elif move == '2' and character['location Y'] < 4:
-            character['location Y'] += 1
-            break
-        elif move == '3' and character['location X'] > 0:
-            character['location X'] -= 1
-            break
-        elif move == '4' and character['location Y'] > 0:
-            character['location Y'] -= 1
-            break
-        else:
-            print('You cannot go that way')
-            continue
-
-    print(character)
-    return character, True
-
-
 def character_has_leveled(character):
     if character['level'] == 1:
         if character['xp'] == 5:
