79372988a71a428233f4a200a91ae0da6c249607
aye-ron98 <aron.zhang013@gmail.com>
2022-11-26 13:07:37 -0800

    Create make_enemy.py, functions related to enemy mechanics

--- battle_mechanics.py
+++ battle_mechanics.py
@@ -7,6 +7,10 @@ functions related to fighting
 - leveling
 - damage
 """
+from itertools import combinations
+from random import randint
+
+import enemy
 
 
 def check_for_challenges(character, board):
@@ -19,66 +23,127 @@ def check_for_challenges(character, board):
 def execute_challenge_protocol(character, board):
     x_y_coordinate = (character['location X'], character['location Y'])
     if board[x_y_coordinate] == 'light challenge':
-        return easy_challenge(character)
-    elif board[x_y_coordinate] == 'medium challenge':
-        return medium_challenge(character)
-    elif board[x_y_coordinate] == 'hard challenge':
-        return hard_challenge(character)
+        return battle(character, enemy.make_enemy(character), roll_initaitve())
 
 
-def easy_challenge(character):
-    answers = [1, 2, 3, 4]
-    for choice, direction in enumerate(answers, 1):
-        print(choice, direction)
+def roll_initaitve():
+    character = randint(1, 10)
+    opponent = randint(1, 10)
 
-    answer = str(input('what is 1 + 1?: '))
-
-    if answer != '2':
-        print('wrong')
-        character['hp'] -= 1
-        return character
+    if character >= opponent:
+        return True
     else:
-        print('correct')
-        character['xp'] += 1
-        return character
+        return False
 
 
-def medium_challenge(character):
-    answers = [1, 2, 3, 4]
-    for choice, direction in enumerate(answers, 1):
-        print(choice, direction)
+def battle(character, opponent, roll_initaitve):
+    player_moves = character['moves'] = battle_cards()
+    enemy_moves = opponent['moves'] = battle_cards()
+    enemy_guard = 0
+    player_guard = 0
+    if roll_initaitve:
+        while character['hp'] != 0 or opponent['hp'] != 0:
+            print('\nQuickly! To battle!\n')  # start of player turn
+            for choice, move in enumerate(player_moves[1], len(player_moves)):
+                print(choice, move)
 
-    answer = str(input('what is 1 + 2?: '))
+            move = int(input('\nwhat is your move?: '))
 
-    if answer != '3':
-        print('wrong')
-        character['hp'] -= 1
-        return character
+            if 0 < move < 5:
+                if player_moves[move][1] < 0:
+                    player_guard = player_moves[move][0]
+                    print('\nYou chose {0}, the next attack will deal {1} less damage!'
+                          .format(player_moves[move], abs(player_moves[move][0])))
+                else:
+                    player_damage = 0
+                    player_damage = player_moves[move][1] - abs(enemy_guard) \
+                        if player_damage > 0 else player_damage == 0
+                    opponent['hp'] -= player_damage
+                    print('\nYou dealt {0} damage to thr enemy!\n'
+                          'They are now at {1} health!'.format(player_damage, opponent['hp']))
+                if opponent['hp'] == 0:
+                    print('Congratulations! You have defeated {}'.format(opponent['name']))
+                    break
+
+            enemy_guard = 0
+            enemy_choice = enemy_moves[randint(1, len(enemy_moves))]  # start of enemy turn
+            print('\nenemy chose {}!\n'.format(enemy_choice[0]))
+
+            if enemy_choice < 0:
+                enemy_guard = enemy_choice[1]
+                print('Your next attack will now do {0} less damage to {1}!\n'
+                      .format((abs(enemy_guard)), opponent['name']))
+            else:
+                enemy_damage = 0
+                enemy_damage = enemy_choice[1] - abs(player_guard) if enemy_damage > 0 else enemy_damage == 0
+                character['hp'] -= enemy_damage
+                print('{0} dealt {1} damage to you! You are now at {2} health!'
+                      .format(opponent['name'], enemy_damage, character['hp']))
+            if character['hp'] == 0:
+                print('you lost!')
+                break
+            player_guard = 0
     else:
-        print('correct')
-        character['xp'] += 1
-        return character
+        while character['hp'] != 0 or opponent['hp'] != 0:
+            enemy_choice = enemy_moves[randint(1, len(enemy_moves))]  # start of enemy turn
+            print('\nenemy chose {}!\n'.format(enemy_choice[0]))
+
+            if enemy_choice < 0:
+                enemy_guard = enemy_choice[1]
+                print('Your next attack will now do {0} less damage to {1}!\n'
+                      .format((abs(enemy_guard)), opponent['name']))
+            else:
+                enemy_damage = 0
+                enemy_damage = enemy_choice[1] - abs(player_guard) if enemy_damage > 0 else enemy_damage == 0
+                character['hp'] -= enemy_damage
+                print('{0} dealt {1} damage to you! You are now at {2} health!'
+                      .format(opponent['name'], enemy_damage, character['hp']))
+            if character['hp'] == 0:
+                print('you lost!')
+                break
+            player_guard = 0
+
+            print("it's your turn now!\n")  # start of player turn
+            for choice, move in enumerate(player_moves[1], len(player_moves)):
+                print(choice, move)
+
+            move = int(input('\nwhat is your move?: '))
+
+            if 0 < move < 5:
+                if player_moves[move][1] < 0:
+                    player_guard = player_moves[move][0]
+                    print('\nYou chose {0}, the next attack will deal {1} less damage!'
+                          .format(player_moves[move], abs(player_moves[move][0])))
+                else:
+                    player_damage = 0
+                    player_damage = player_moves[move][1] - abs(enemy_guard) \
+                        if player_damage > 0 else player_damage == 0
+                    opponent['hp'] -= player_damage
+                    print('\nYou dealt {0} damage to thr enemy!\n'
+                          'They are now at {1} health!'.format(player_damage, opponent['hp']))
+                if opponent['hp'] == 0:
+                    print('Congratulations! You have defeated {}'.format(opponent['name']))
+                    break
+            enemy_guard = 0
+
+    print('the battle is over! You are now at {} health'.format(character['hp']))
+    character['moves'] = []
+    return character
 
 
-def hard_challenge(character):
-    answers = [5, 10, 15, 20]
-    for choice, direction in enumerate(answers, 1):
-        print(choice, direction)
+def battle_cards():
+    attacks = {'strike': 5, 'stab': 8, 'scare': 3, 'drop kick': 10, 'bite': 2, 'arrow': 10, 'ultimate': 100}
+    defense = {'defend': -5, 'parry': -3, 'dodge': -10, 'guard': -8, 'garrison': -6}
 
-    answer = str(input('what is 5 + 5?: '))
+    attack_combos = list(combinations(attacks.items(), 3))
+    defense_combos = list(combinations(defense.items(), 2))
 
-    if answer != '10':
-        print('wrong')
-        character['hp'] -= 1
-        return character
-    else:
-        print('correct')
-        character['xp'] += 1
-        return character
+    return attack_combos[randint(1, len(attack_combos))] + defense_combos[randint(1, len(defense_combos))]
 
 
 def main():
-    main()
+    # main()
+    battle_cards()
 
 
 if __name__ == '__main__':
