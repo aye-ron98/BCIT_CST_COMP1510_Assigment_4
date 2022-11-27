bd8625a6da1d4d7f9224910efb0a31e7badcf5d4
aye-ron98 <aron.zhang013@gmail.com>
2022-11-26 17:17:43 -0800

    refactor battle function to be more atomic, split player choice and damage into helper functions

--- battle_mechanics.py
+++ battle_mechanics.py
@@ -31,108 +31,109 @@ def roll_initaitve():
     opponent = randint(1, 10)
 
     if character >= opponent:
+        print('\nYou are lucky today, you will attack first!\n')
         return True
     else:
+        print('\nnot so lucky, your enemy will attack first!\n')
         return False
 
 
-def battle(character, opponent, roll_initaitve):
-    player_moves = character['moves'] = battle_cards()
-    enemy_moves = opponent['moves'] = battle_cards()
-    enemy_guard = 0
+def battle(character, opponent, player_goes_first):
+    character['moves'] = battle_cards()
+    opponent['moves'] = battle_cards()
     player_guard = 0
-    if roll_initaitve:
-        while character['hp'] != 0 or opponent['hp'] != 0:
-            print('\nQuickly! To battle!\n')  # start of player turn
-            for choice in range(0, len(player_moves)):
-                print(choice + 1, player_moves[choice][0])
-
-            move = int(input('\nwhat is your move?: ')) - 1
-
-            if 0 < move < len(player_moves) + 1:
-                if player_moves[move][1] < 0:
-                    player_guard = abs(player_moves[move][1])
-                    print('\nYou chose {0}, the next attack will deal {1} less damage!'
-                          .format(player_moves[move][0], player_guard))
-                else:
-                    player_damage = 0
-                    player_damage = player_moves[move][0] - abs(enemy_guard) \
-                        if player_damage > 0 else player_damage == 0
-                    opponent['hp'] -= player_damage
-                    print('\nYou dealt {0} damage to thr enemy!\n'
-                          'They are now at {1} health!'.format(player_damage, opponent['hp']))
-                if opponent['hp'] == 0:
-                    print('Congratulations! You have defeated {}'.format(opponent['name']))
+    enemy_guard = 0
+    if player_goes_first:
+        while True:
+            player_choice = player_attack(character, enemy_guard)
+            if player_choice < 0:
+                player_guard += player_choice
+            else:
+                opponent['hp'] -= player_choice
+                print('{1} is now at {0} health!\n'.format(opponent['hp'], opponent['name']))
+                if opponent['hp'] <= 0:
+                    print('{0} has been defeated! You gain 1 xp. You are at {1} health.'
+                          .format(opponent['name'], character['hp']))
                     break
-            else:
-                print('that is not a valid move!')
-
             enemy_guard = 0
-            enemy_choice = enemy_moves[randint(1, len(enemy_moves))]  # start of enemy turn
-            print('\nenemy chose {}!\n'.format(enemy_choice[0]))
-
-            if enemy_choice[1] < 0:
-                enemy_guard = abs(enemy_choice[1])
-                print('Your next attack will now do {0} less damage to {1}!\n'
-                      .format(enemy_guard, opponent['name']))
+            enemy_choice = enemy_attack(opponent, player_guard)
+            if enemy_choice < 0:
+                enemy_guard += enemy_choice
             else:
-                enemy_damage = 0
-                enemy_damage = enemy_choice[1] - player_guard if enemy_damage > 0 else enemy_damage == 0
-                character['hp'] -= enemy_damage
-                print('{0} dealt {1} damage to you! You are now at {2} health!'
-                      .format(opponent['name'], enemy_damage, character['hp']))
-            if character['hp'] == 0:
-                print('you lost!')
-                break
+                character['hp'] -= enemy_choice
+                print('You are now at {0} health!\n'.format(character['hp']))
+                if character['hp'] <= 0:
+                    print('You are defeated')
+                    break
             player_guard = 0
     else:
-        while character['hp'] != 0 or opponent['hp'] != 0:
-            enemy_choice = enemy_moves[randint(1, len(enemy_moves))]  # start of enemy turn
-            print('\nenemy chose {}!\n'.format(enemy_choice[0]))
-
-            if enemy_choice[1] < 0:
-                enemy_guard = abs(enemy_choice[1])
-                print('Your next attack will now do {0} less damage to {1}!\n'
-                      .format(enemy_guard, opponent['name']))
+        while True:
+            enemy_choice = enemy_attack(opponent, player_guard)
+            if enemy_choice < 0:
+                enemy_guard += enemy_choice
             else:
-                enemy_damage = 0
-                enemy_damage = enemy_choice[1] - player_guard if enemy_damage > 0 else enemy_damage == 0
-                character['hp'] -= enemy_damage
-                print('{0} dealt {1} damage to you! You are now at {2} health!'
-                      .format(opponent['name'], enemy_damage, character['hp']))
-            if character['hp'] == 0:
-                print('you lost!')
-                break
-            player_guard = 0
-
-            print("it's your turn now!\n")  # start of player turn
-            for choice in range(0, len(player_moves)):
-                print(choice + 1, player_moves[choice][0])
-
-            move = int(input('\nwhat is your move?: ')) - 1
-
-            if 0 < move < len(player_moves) + 1:
-                if player_moves[move][1] < 0:
-                    player_guard = abs(player_moves[move][1])
-                    print('\nYou chose {0}, the next attack will deal {1} less damage!'
-                          .format(player_moves[move][0], player_guard))
-                else:
-                    player_damage = 0
-                    player_damage = player_moves[move][1] - enemy_guard \
-                        if player_damage > 0 else player_damage == 0
-                    opponent['hp'] -= player_damage
-                    print('\nYou dealt {0} damage to thr enemy!\n'
-                          'They are now at {1} health!'.format(player_damage, opponent['hp']))
-                if opponent['hp'] == 0:
-                    print('Congratulations! You have defeated {}'.format(opponent['name']))
+                character['hp'] -= enemy_choice
+                print('You are now at {0} health!\n'.format(character['hp']))
+                if character['hp'] <= 0:
+                    print('You are defeated')
                     break
+            player_guard = 0
+            player_choice = player_attack(character, enemy_guard)
+            if player_choice < 0:
+                player_guard += player_choice
             else:
-                print('that is not a valid move!')
+                opponent['hp'] -= player_choice
+                print('{1} is now at {0} health!\n'.format(opponent['hp'], opponent['name']))
+                if opponent['hp'] <= 0:
+                    print('{0} has been defeated! You gain 1 xp. You are at {1} health.'
+                          .format(opponent['name'], character['hp']))
+                    break
             enemy_guard = 0
 
     print('the battle is over! You are now at {} health'.format(character['hp']))
     character['moves'] = []
     return character
+
+
+def player_attack(character, enemy_guard):
+    cards_on_hand = character['moves']
+    print('\nQuickly! To battle!\n')  # start of player turn
+    while True:
+        for choice in range(0, len(cards_on_hand)):
+            print(choice + 1, cards_on_hand[choice][0])
+
+        move = int(input('what is your move?: ')) - 1
+
+        if 0 <= move < len(cards_on_hand):
+            if cards_on_hand[move][1] < 0:
+                player_guard = cards_on_hand[move][1]
+                print('You chose {0}, the next attack will deal {1} less damage!'
+                      .format(cards_on_hand[move][0], abs(player_guard)))
+                return player_guard
+            else:
+                player_damage = cards_on_hand[move][1] - abs(enemy_guard) \
+                    if cards_on_hand[move][1] - abs(enemy_guard) > 0 else 0
+                print('You chose {1} and dealt {0} damage.'.format(player_damage, cards_on_hand[move][0]), end='')
+            return player_damage
+        else:
+            print('that is not a valid move!')
+            continue
+
+
+def enemy_attack(opponent, player_guard):
+    cards_on_hand = opponent['moves']
+    enemy_choice = cards_on_hand[randint(0, len(cards_on_hand) - 1)]
+    print('\n{0} chose {1}!\n'.format(opponent['name'], enemy_choice[0]))
+
+    if enemy_choice[1] < 0:
+        enemy_guard = enemy_choice[1]
+        print('Your next attack will now do {0} less damage to {1}!\n'
+              .format(abs(enemy_guard), opponent['name']))
+        return enemy_guard
+    else:
+        enemy_damage = enemy_choice[1] - player_guard if enemy_choice[1] - player_guard > 0 else 0
+        print('{0} dealt {1} damage to you!'.format(opponent['name'], enemy_damage), end='')
+        return enemy_damage
 
 
 def battle_cards():
