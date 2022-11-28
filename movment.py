1316e8c4b7c11ac2f68ea114ad42f64144c384f9
aye-ron98 <aron.zhang013@gmail.com>
2022-11-27 19:37:33 -0800

    Refactor get_user_choice to be more atomic
    Add helper functions validate move and validate location

--- movment.py
+++ movment.py
@@ -7,37 +7,69 @@ functions related to moving around the map
 
 def get_user_choice(character: dict) -> dict:
     """
-    Take user input and verify input is in range [0, 4]. Updates relevant character values upon verification.
+    Take user input modifies character 'location' key.
 
     :param character: a dictionary
-    :preconditon character: must contain key 'location X' and 'location Y'
+    :preconditon character: must contain key 'location'
     :preconditon character: values must be positive integers
     :postcondition: updates relevant character keys
     :return: the character dictionary
     """
-    directions = ['move south', 'move east', 'move north', 'move west']
-    for choice, direction in enumerate(directions, 1):
+    directions = [('move north', (0, 1)), ('move east', (1, 0)), ('move south', (0, -1)), ('move west', (-1, 0))]
+    for choice, direction in enumerate(directions[0], 1):
         print(choice, direction)
 
     while True:
-        move = input('What direction would you like to move?')
-        if move == '1' and character['location X'] < 4:
-            character['location X'] += 1
-            break
-        elif move == '2' and character['location Y'] < 4:
-            character['location Y'] += 1
-            break
-        elif move == '3' and character['location X'] > 0:
-            character['location X'] -= 1
-            break
-        elif move == '4' and character['location Y'] > 0:
-            character['location Y'] -= 1
-            break
+        move = validate_move(1, 4)
+        potential_move = validate_location(int(move), directions, character)
+        if 0 <= potential_move[0] <= 4 and 0 <= potential_move[1] <= 4:
+            character['location'] = potential_move
+            return character
         else:
-            print('You cannot go that way')
-            continue
+            print('{} you cannot go that way'.format(character['name']))
 
-    return character
+
+def validate_move(lower_bound: int, upperbound: int) -> str:
+    """
+    Takes user input and validates it is within the range of upper and lower bound. Asks user to try again if input is
+    not in a valid range.
+
+    Converts upper and lower bound to string
+    :param lower_bound: an integer
+    :param upper upperbound: an integer
+    :precondition upperbound: must be greater than lower bound
+    :postcondition: if user input is in [lower_bound, upper_bound] will return user input
+    :return: user input as a string
+    """
+
+    user_input = input('What is your choice?: ')
+
+    while True:
+        if str(lower_bound) <= user_input <= str(upperbound):
+            return user_input
+        else:
+            print('That is not an option, try again!')
+
+
+def validate_location(choice: int, options: list, player: dict) -> tuple:
+    """
+    Sum individual values two tuples together. Tuple values must be integer
+
+    :param choice: a nonzero positive integer
+    :param options: a list of length 2
+    :param player: a dictionary
+    :precondition options: second item in lost must be tuple of length 2, contents must be integers
+    :precondition player: must contain key 'location' value must be tuple of length 2, contents must be integers
+    :postcondition: will sum individual values two tuples together
+    :return: a tuple length 2
+    >>> validate_location(1, [('move south', (0, -1))], {'location': (3, 4)})
+    (3, 3)
+    """
+    new_coordinates = options[choice - 1][1]
+    player_coordinates = player['location']
+    updated_coordinates = [new_coordinates + player_coordinates for
+                           new_coordinates, player_coordinates in zip(new_coordinates, player_coordinates)]
+    return tuple(updated_coordinates)
 
 
 def main():
