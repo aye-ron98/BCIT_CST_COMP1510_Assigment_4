f2b14dc309d6d7b45bc3b4a7d4d19a83fe2cc308
aye-ron98 <aron.zhang013@gmail.com>
2022-11-30 09:41:17 -0800

    complete make_board_unit tests

---
+++ world_building_test/test_make_board.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from world_building import make_board
+from unittest.mock import patch
+
+
+class TestMakeBoard(TestCase):
+
+    def test_make_board(self):
+        actual = make_board(5, 5)
+        self.assertEqual(25, len(actual.keys()))
+
+    def test_make_board_1_by_1(self):
+        actual = make_board(1, 1)
+        self.assertEqual(1, len(actual.keys()))
