807a40f33b273c1d68c72967ececfb3fc6c4b451
aye-ron98 <aron.zhang013@gmail.com>
2022-11-29 23:46:22 -0800

    Complete test treasure unit test

---
+++ test_treasure.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from unittest.mock import patch
+from scenarios import treasure
+import io
+from random import randint
+
+
+class TestTreasure(TestCase):
+
+    @patch('random.randint', return_value=0)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_treasure_min(self, mock_print, mock_number):
+        actual = treasure({'damage': 0})
+        the_game_printed_this = mock_print.getvalue()
+        expected_output = "\nIt's your luck day!, You stumble across an armory, you leave with a stick. " \
+                          "ALl damage is now increased by +3.\n"
+        self.assertEqual(actual, {'damage': 3})
+        self.assertEqual(expected_output, the_game_printed_this)
+
+    @patch('random.randint', return_value=2)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_treasure(self, mock_print, mock_number):
+        actual = treasure({'damage': 0})
+        the_game_printed_this = mock_print.getvalue()
+        expected_output = "\nIt's your luck day!, You stumble across an armory, you leave with a battering ram. " \
+                          "ALl damage is now increased by +5.\n"
+        self.assertEqual(actual, {'damage': 5})
+        self.assertEqual(expected_output, the_game_printed_this)
+
+    @patch('random.randint', return_value=4)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_treasure_min(self, mock_print, mock_number):
+        actual = treasure({'damage': 0})
+        the_game_printed_this = mock_print.getvalue()
+        expected_output = "\nIt's your luck day!, You stumble across an armory, you leave with a flag pole. " \
+                          "ALl damage is now increased by +7.\n"
+        self.assertEqual(actual, {'damage': 7})
+        self.assertEqual(expected_output, the_game_printed_this)
+
+
+
+
