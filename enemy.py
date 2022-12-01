1946d77536617c6b6591c08f2e2d8de75e416d29
aye-ron98 <aron.zhang013@gmail.com>
2022-11-30 17:51:26 -0800

    Edit error in additional_enemy_characteristics
    if statement now evaluates is condition is > 0

--- enemy.py
+++ enemy.py
@@ -52,13 +52,13 @@ def additional_enemy_characteristics(opponent: dict) -> None:
 
     :param opponent: a dictionary
     :precondition opponent: must be a dictionary
-    :postcondition: if key 'damage' in opponent will print a statement indicating so
-    :postcondition: if key 'defence' in opponent will print a statement indicating so
+    :postcondition: if key 'damage' in opponent > 0 will print a statement indicating so
+    :postcondition: if key 'defence' in opponent > 0 will print a statement indicating so
     """
-    if 'damage' in opponent:
+    if opponent['damage'] > 0:
         print('Watch out! {0} has been hitting the gym! All their attacks will have a +{1} effect!'
               .format(opponent['name'], opponent['damage']))
-    elif 'defence' in opponent:
+    elif opponent['defence'] > 0:
         print('{0} has been meditating lately, all your attacks will have a -{1} effect!'
               .format(opponent['name'], opponent['damage']))
 
