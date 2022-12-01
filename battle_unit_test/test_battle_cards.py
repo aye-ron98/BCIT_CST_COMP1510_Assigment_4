b527c49c1483ddd5889e31d804b5b43e0a243cfe
aye-ron98 <aron.zhang013@gmail.com>
2022-12-01 15:21:11 -0800

    replace patch with _ as parameters to satisfy PEP8

--- battle_unit_test/test_battle_cards.py
+++ battle_unit_test/test_battle_cards.py
@@ -6,16 +6,16 @@ from unittest.mock import patch
 class TestBattleCards(TestCase):
 
     @patch('random.randint', return_value=2)
-    def test_battle(self, mock_roll):
+    def test_battle(self, _):
         actual = battle_cards()
         self.assertEqual((('strike', 5), ('stab', 8), ('bite', 2), ('defend', -5), ('guard', -8)), actual)
 
     @patch('random.randint', return_value=1)
-    def test_battle_roll_1(self, mock_roll):
+    def test_battle_roll_1(self, _):
         actual = battle_cards()
         self.assertEqual((('strike', 5), ('stab', 8), ('drop kick', 10), ('defend', -5), ('dodge', -10)), actual)
 
     @patch('random.randint', return_value=9)
-    def test_battle_roll_upper_bound_9(self, mock_roll):
+    def test_battle_roll_upper_bound_9(self, _):
         actual = battle_cards()
         self.assertEqual((('strike', 5), ('drop kick', 10), ('bite', 2), ('guard', -8), ('garrison', -6)), actual)
