70a14c531447a47d946ac8863517cc341aa5c5e3
aye-ron98 <aron.zhang013@gmail.com>
2022-11-30 18:28:58 -0800

    refactor execute glow up protocol, user input now used movment.validate_move

--- character_condition.py
+++ character_condition.py
@@ -6,6 +6,7 @@ functions that check character status
 - health
 -xp
 """
+import movment
 
 
 def character_has_leveled(character: dict) -> dict:
@@ -71,33 +72,33 @@ def execute_glow_up_protocol(character: dict) -> dict:
     Update character dictionary values
 
     :param character: a dictionary
-    :precondition character: must contain keys 'glow up', 'damage', 'defence' 'hp'
+    :precondition character: must contain keys 'glow up', 'damage', 'defence' 'hp', 'name'
     :postcondition: will update character values pending user input
     :return: the dictionary character
     """
     if character['glow up']:
-        print('\nCongratulations! you have leveled up!'
-              'choose an upgradable from the following!\n')
+        print('\nCongratulations! {} have leveled up!'
+              'choose an upgradable from the following!\n'.format(character['name']))
         character['glow up'] = False
         upgradeables = ['+10 health', '+10 base damage', '+10 base defence']
         for choice, item in enumerate(upgradeables, 1):
             print(item, ': ', choice)
 
-        user_choice = str(input('\nwhat would you like?: '))
+        user_choice = movment.validate_move(1, 3)
 
         if user_choice == '1':
-            print('\nYour health is now permanently increased by 10, Your total health is now {}'
-                  .format(character['hp'] + 10))
             character['hp'] += 10
+            print('\nYour health is now permanently increased by 10, Your total health is now {0}'
+                  .format(character['hp']))
         if user_choice == '2':
-            print('\nAll attacks now have a +10 attack on top of their normal stats, beware enemy defences, they may '
-                  'still defend against you!')
             character['damage'] += 10
+            print('\nAll attacks now have a +10 attack on top of their normal stats, beware enemy defences, they may'
+                  ' still defend against you, your total attack is now {0}'.format(character['damage']))
         if user_choice == '3':
-            print('\nAll incoming attacks are now reduced by 10!')
             character['defence'] += 10
+            print('\nAll incoming attacks are now reduced by 10!, your total defence is now {0}'
+                  .format(character['defence']))
 
-    print(character)
     return character
 
 
