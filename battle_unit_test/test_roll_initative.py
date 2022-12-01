3b1024211a8b21c5056c170a68c333147c9ff466
aye-ron98 <aron.zhang013@gmail.com>
2022-11-30 19:03:32 -0800

    Complete roll iniative unit test

---
+++ battle_unit_test/test_roll_initative.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from unittest.mock import patch
+from battle_mechanics import roll_initaitve
+import io
+
+
+class TestRollIniative(TestCase):
+
+    @patch('random.randint', return_value=1)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_roll_initaitve_True(self, mock_print, mock_roll):
+        actual = roll_initaitve()
+        expected = '\nYou are lucky today, you will attack first!\n'
+        the_game_printed_this = mock_print.getvalue()
+        self.assertEqual(True, actual)
+        self.assertEqual(expected, the_game_printed_this)
+
+    @patch('random.randint', return_value=2)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_roll_initaitve_False(self, mock_print, mock_roll):
+        actual = roll_initaitve()
+        expected = '\nnot so lucky, your enemy will attack first!\n'
+        the_game_printed_this = mock_print.getvalue()
+        self.assertEqual(False, actual)
+        self.assertEqual(expected, the_game_printed_this)
