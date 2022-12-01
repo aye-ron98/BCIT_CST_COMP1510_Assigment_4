6ce802f71a77a6d33a3aa8f12a8a0a72d1279528
aye-ron98 <aron.zhang013@gmail.com>
2022-11-30 16:33:05 -0800

    Complete make_enemy unit test

---
+++ enemy_unit_test/test_make_enemy.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import patch
+from enemy import make_enemy
+
+
+class TestMakeEnemy(TestCase):
+
+    @patch('enemy.enemy_name', return_value='Rue Pual')
+    def test_make_enemy(self, mock_name):
+        actual = make_enemy({'level': 1, 'hp': 10, 'xp': 1})
+        self.assertEqual({'damage': 0, 'defence': 0, 'hp': 4.0, 'name': 'Rue Pual'}, actual)
+
+    @patch('enemy.enemy_name', return_value='Rue Pual')
+    @patch('random.randint', return_value=2)
+    def test_make_enemy_player_level_2(self, mock_damage, mock_name):
+        actual = make_enemy({'level': 2, 'hp': 10, 'xp': 1})
+        self.assertEqual({'damage': 2, 'defence': 0, 'hp': 35, 'name': 'Rue Pual'}, actual)
+
+    @patch('enemy.enemy_name', return_value='Rue Pual')
+    @patch('random.randint', side_effect=[0, 2])
+    def test_make_enemy_player_level_2_no_damage(self, mock_damage, mock_name):
+        actual = make_enemy({'level': 2, 'hp': 10, 'xp': 1})
+        self.assertEqual({'damage': 0, 'defence': 2, 'hp': 35, 'name': 'Rue Pual'}, actual)
+
+
+
