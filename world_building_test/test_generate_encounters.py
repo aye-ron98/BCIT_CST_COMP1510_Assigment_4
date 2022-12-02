c839647534c8e7cf15995ea1685e76b93cb6233f
aye-ron98 <aron.zhang013@gmail.com>
2022-12-01 18:00:15 -0800

    replace patch with _ as parameters to satisfy PEP8

--- world_building_test/test_generate_encounters.py
+++ world_building_test/test_generate_encounters.py
@@ -6,33 +6,31 @@ from world_building import generate_encounters
 class TestGenerateEncounters(TestCase):
 
     @patch('random.randint', return_value=1)
-    def test_generate_encounters_health(self, mock_number):
+    def test_generate_encounters_health(self, _):
         actual = generate_encounters()
         self.assertEqual('health', actual)
 
     @patch('random.randint', return_value=2)
-    def test_generate_encounters_damage(self, mock_number):
+    def test_generate_encounters_damage(self, _):
         actual = generate_encounters()
         self.assertEqual('damage', actual)
 
     @patch('random.randint', return_value=3)
-    def test_generate_encounters_defence(self, mock_number):
+    def test_generate_encounters_defence(self, _):
         actual = generate_encounters()
         self.assertEqual('defense', actual)
 
     @patch('random.randint', return_value=4)
-    def test_generate_encounters_puzzle(self, mock_number):
+    def test_generate_encounters_puzzle(self, _):
         actual = generate_encounters()
         self.assertEqual('puzzle', actual)
 
     @patch('random.randint', return_value=5)
-    def test_generate_encounters_riddle(self, mock_number):
+    def test_generate_encounters_riddle(self, _):
         actual = generate_encounters()
         self.assertEqual('riddle', actual)
 
     @patch('random.randint', return_value=6)
-    def test_generate_encounters_battle(self, mock_number):
+    def test_generate_encounters_battle(self, _):
         actual = generate_encounters()
         self.assertEqual('battle', actual)
-
-
