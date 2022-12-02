fa2a2cce333e57582ecd43d5e735bd869b6f7774
aye-ron98 <aron.zhang013@gmail.com>
2022-12-01 17:55:52 -0800

    replace patch with _ as parameters to satisfy PEP8

--- enemy_unit_test/test_enemy_name.py
+++ enemy_unit_test/test_enemy_name.py
@@ -6,16 +6,16 @@ from enemy import enemy_name
 class TestEnemyName(TestCase):
 
     @patch('random.randint', return_value=3)
-    def test_enemy_name(self, mock_random):
+    def test_enemy_name(self, _):
         actual = enemy_name()
         self.assertEqual('rue paul', actual)
 
     @patch('random.randint', return_value=1)
-    def test_enemy_name_boundry_start(self, mock_random):
+    def test_enemy_name_boundry_start(self, _):
         actual = enemy_name()
         self.assertEqual('bob', actual)
 
     @patch('random.randint', return_value=7)
-    def test_enemy_boundry_end(self, mock_random):
+    def test_enemy_boundry_end(self, _):
         actual = enemy_name()
         self.assertEqual('uncle sam', actual)
