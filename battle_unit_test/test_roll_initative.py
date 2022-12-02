7118d806381a862797a159012afdb047608fab08
aye-ron98 <aron.zhang013@gmail.com>
2022-12-01 18:01:16 -0800

    replace patch with _ as parameters to satisfy PEP8

--- battle_unit_test/test_roll_initative.py
+++ battle_unit_test/test_roll_initative.py
@@ -8,7 +8,7 @@ class TestRollIniative(TestCase):
 
     @patch('random.randint', return_value=1)
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_roll_initaitve_True(self, mock_print, mock_roll):
+    def test_roll_initaitve_True(self, mock_print, _):
         actual = roll_initaitve()
         expected = '\nYou are lucky today, you will attack first!\n'
         the_game_printed_this = mock_print.getvalue()
@@ -17,7 +17,7 @@ class TestRollIniative(TestCase):
 
     @patch('random.randint', return_value=2)
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_roll_initaitve_False(self, mock_print, mock_roll):
+    def test_roll_initaitve_False(self, mock_print, _):
         actual = roll_initaitve()
         expected = '\nnot so lucky, your enemy will attack first!\n'
         the_game_printed_this = mock_print.getvalue()
